035bd40b53078395a25f7c175021ed465c58428b
kaibalya jena <kaibalya18j@gmail.com>
2022-12-20 21:36:08 +0530

    register api working

--- api/index.js
+++ api/index.js
@@ -2,17 +2,16 @@ const express = require('express');
 const app = express();
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
-
+const authRoute = require("./routes/auth")
 dotenv.config()
+app.use(express.json())
 
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URL,{
 }).then(console.log("connected to mongodb")).catch(err=>console.log(err));
 
 
-app.use("/hello",(req,res)=>{
-    console.log("working on the helloo hello website")
-})
+app.use("/api/auth",authRoute)
 
 app.listen("3000",()=>{
     console.log("the backend is running hello my friends what is up bro")
