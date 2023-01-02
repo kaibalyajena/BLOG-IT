750a98c2f98189519302588135573c41bb5b1e05
kaibalya jena <kaibalya18j@gmail.com>
2023-01-02 11:40:55 +0530

    topbar completed

--- client/src/topbar/TopBar.jsx
+++ client/src/topbar/TopBar.jsx
@@ -2,6 +2,26 @@ import "./topbar.css"
 
 export default function TopBar() {
   return (
-    <div className="top">TopBar</div>
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-square-facebook"></i>
+        <i className="topIcon fa-brands fa-square-twitter"></i>
+        <i className="topIcon fa-brands fa-square-pinterest"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img className="topImage" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="profile"></img>
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
   )
 }
