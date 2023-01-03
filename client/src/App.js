0ef11a327928fc5f9b6ef119bcd1f82f9471ba94
kaibalya jena <kaibalya18j@gmail.com>
2023-01-03 15:33:26 +0530

    basic routes created using reaact route dom

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,27 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <>
+    <Router>
     <TopBar/>
-    <Register/>
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/post/:postId" element={<Single/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+        <Route path="/write" element={<Write/>}/>
+      </Routes>
+    </Router>
   );
 }
 
