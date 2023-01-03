82834670bfa1344eb3e62571d3317421e1d6acaa
kaibalya jena <kaibalya18j@gmail.com>
2023-01-03 21:11:11 +0530

    registration process complete

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,22 +1,53 @@
 import "./register.css"
 import {Link} from "react-router-dom"
+import { useState } from "react"
+import axios from "axios"
 
 export default function Register() {
+  const [username,setUsername] = useState([])
+  const [password,setPassword] = useState([])
+  const [email,setEmail] = useState([])
+  const [error,setError] = useState([])
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError(false)
+    try{
+      const res = await axios.post("/auth/register",{
+        username,email,password
+      })
+      console.log(res)
+      {res.data && window.location.replace("/login")}
+    }catch(err){
+      console.log(err)
+      setError(true)
+    }
+    
+  }
+
   return (
     <div className='register'>
         <span className="registerTitle">Register</span>
-        <form action="" className="registerForm">
+        <form action="" className="registerForm" onSubmit={handleSubmit}>
         <label htmlFor="">Username</label>
-            <input type="text" placeholder="Enter your username" />
+            <input type="text" placeholder="Enter your username" 
+            onChange={e=>setUsername(e.target.value)}
+            />
             <label htmlFor="">Email</label>
-            <input type="text" placeholder="Enter your email" />
+            <input type="text" placeholder="Enter your email" 
+            onChange={e=>setEmail(e.target.value)}
+            />
             <label htmlFor="">Password</label>
-            <input type="text" placeholder="password" />
-            <button>Register</button>
+            <input type="password" placeholder="password"
+            onChange={e=>setPassword(e.target.value)} 
+            />
+            <button type="submit">Register</button>
         </form>
         <button className="registerLoginButton">
           <Link className="link" to="/login">Login</Link>
         </button>
+        {error===true && (<span className="error">something went wrong !!</span>)}
+        
     </div>
   )
 }
