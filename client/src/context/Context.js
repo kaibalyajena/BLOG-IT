c225e6d23085be990efb9c5ffafc0e3592b07266
kaibalya jena <kaibalya18j@gmail.com>
2023-01-04 11:04:34 +0530

    login using context api done

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -10,14 +10,14 @@ const INITIAL_STATE = {
 export const Context = createContext(INITIAL_STATE)
 
 export const ContextProvider = ({children}) =>{
-    const [state,dispath] = useReducer(Reducer,INITIAL_STATE)
+    const [state,dispatch] = useReducer(Reducer,INITIAL_STATE)
 
     return (
         <Context.Provider value = {{
             user:state.user,
             isFetching:state.isFetching,
             error:state.error,
-            dispath
+            dispatch
         }}>
             {children}
         </Context.Provider>
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,16 +1,41 @@
 import "./login.css"
 import {Link} from "react-router-dom"
+import { useRef } from "react"
+import { useContext } from "react"
+import { Context } from "../../context/Context"
+import axios from "axios"
 
 export default function Login() {
+  const userRef = useRef()
+  const passwordRef = useRef()
+  const {user,dispatch,isFetching} = useContext(Context)
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    dispatch({type:"LOGIN_START"})
+    try{
+      const res = await axios.post("/auth/login",{
+        username:userRef.current.value,
+        password:passwordRef.current.value
+      })
+      dispatch({type:"LOGIN_SUCCESS",payload:res.data})
+    }catch(err){
+      dispatch({type:"LOGIN_FAILURE"})
+    }
+  }
+
+  console.log(user)
+  console.log(isFetching)
+
   return (
     <div className='login'>
         <span className="loginTitle">Login</span>
-        <form action="" className="loginForm">
-            <label htmlFor="">Email</label>
-            <input type="text" placeholder="Enter your email" />
+        <form action="" className="loginForm" onSubmit={handleSubmit}>
+            <label htmlFor="">Username</label>
+            <input type="text" placeholder="Enter your username" ref={userRef}/>
             <label htmlFor="">Password</label>
-            <input type="text" placeholder="password" />
-            <button>login</button>
+            <input type="password" placeholder="password" ref={passwordRef}/>
+            <button type="submit">login</button>
         </form>
         <button className="loginRegisterButton">
           <Link to="/register" className="link">Register</Link>
