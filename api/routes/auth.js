bcec4fddcc0c4fc8f8c0cd80c9bfe350bb96abf3
kaibalya jena <kaibalya18j@gmail.com>
2023-01-19 23:04:54 +0530

    notion notes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -40,4 +40,38 @@ router.post("/login",async (req,res)=>{
 
 
 
-module.exports=router
+// module.exports=router
+
+// command to install - npm i express mongoose dotenv multer
+
+// express for server
+
+// mongoose for connecting with mongodb database
+
+// dotenv for hiding the credentials in the folder of mongodb cloud
+
+// multer for adding and handling photos and all.
+
+// In mongodb altas we need to create a shared cluster and then connect with your application
+
+// postman is used for api testing
+
+// npm init to install node js
+
+// control c to exit npm start from the terminal
+
+// nodemon is used to change the terminal while running npm start whenever we change something
+
+// .env file is used to hide something like database url etc.
+
+// bcrypt library used to encrypt passwords that go in the database
+
+// jwt — json web talking
+
+// `sudo killall -9 node` — this command is used to kill all node express servers( when error occurs that server is already in use)
+
+// multer — used for adding images in out api
+
+// npm i react-router-dom — for the routes and stuff
+
+// axios library is used to fetch data
